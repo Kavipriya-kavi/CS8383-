6db71a7c69c34263f5ba47048bcfa5d6672b5e05
Kavipriya-kavi <89631764+Kavipriya-kavi@users.noreply.github.com>
2021-09-24 16:18:38 +0530

    Add files via upload

---
+++ program52.java
@@ -0,0 +1,9 @@
+public	class A{  
+	public static void main(String args[]){  
+	StringBuilder sb=new StringBuilder("Hello ");  
+	sb.append("Java");//now original string is changed  
+	System.out.println(sb);//prints Hello Java  
+	}  
+	}  
+
+ 
